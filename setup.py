af282ca95b3274499ee5dfebbec6c0e13ef46248
Marius Cobzarenco <marius@reinfer.io>
2014-07-18 11:29:04 +0100

    setup.py in the reinfer.io style

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from setuptools import setup
 import strconv
 
-kwargs = {
-    'py_modules': ['strconv'],
-    'test_suite': 'test_strconv',
-    'name': 'strconv',
-    'version': strconv.__version__,
-    'author': 'Byron Ruth',
-    'author_email': 'b@devel.io',
-    'description': 'String type inference and conversion',
-    'license': 'BSD',
-    'keywords': 'types inference conversion strings',
-    'url': 'https://github.com/bruth/strconv/',
-    'classifiers': [
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-    ],
-}
-
-setup(**kwargs)
+setup(name='strconv',
+      version=strconv.__version__,
+      author='Byron Ruth',
+      author_email='b@devel.io',
+      description='String type inference and conversion',
+      license='BSD',
+      url='https://github.com/mcobzarenco/strconv',
+      test_suite='test_strconv',
+      install_requires=[
+          'python-dateutil>=2.2',
+      ],
+      py_modules=['strconv'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: BSD License',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+      ],
+)
